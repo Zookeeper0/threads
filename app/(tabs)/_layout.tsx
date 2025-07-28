fa9edebb49784ba878cf64f424fce807b77c8a14
eastZoo <dongju010@naver.com>
2025-07-28 23:02:23 +0900

    UPDATE : 하단 탭바 커스터 마이징

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,78 @@
+import { Ionicons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
 
+/**
+ * - 아이콘 참고 사이트
+ * https://icons.expo.fyi/Index
+ */
 export default function TabsLayout() {
-  return <Tabs />;
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          tabBarLabel: () => null,
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name="home"
+              size={24}
+              color={focused ? "black" : "gray"}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          tabBarLabel: () => null,
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name="search"
+              size={24}
+              color={focused ? "black" : "gray"}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="add"
+        options={{
+          tabBarLabel: () => null,
+          tabBarIcon: ({ focused }) => (
+            <Ionicons name="add" size={24} color={focused ? "black" : "gray"} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="activity"
+        options={{
+          tabBarLabel: () => null,
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name="heart-outline"
+              size={24}
+              color={focused ? "black" : "gray"}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="[username]"
+        options={{
+          tabBarLabel: () => null,
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name="person-outline"
+              size={24}
+              color={focused ? "black" : "gray"}
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 }
