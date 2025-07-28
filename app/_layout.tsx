1bc15c75ab9bd7591f683347181a97e1591e05e2
eastZoo <dongju010@naver.com>
2025-07-28 23:12:07 +0900

    UPDATE : 탭도 스택으로 관리 가능

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -13,6 +13,8 @@ export default function RootLayout() {
         headerShown: false,
       }}
     >
+      {/* 아래 코드 적용시 탭 레이아웃도 스택처럼 쌓이게 됨 */}
+      <Stack.Screen name="(tabs)" />
       {/* 모달 페이지는 스택 레이아웃에 포함되지 않음 
       모달 표현 방식으로 모달을 띄우게 부모 레이아웃에서 띄우게 함*/}
       <Stack.Screen name="modal" options={{ presentation: "modal" }} />
