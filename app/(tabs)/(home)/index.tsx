22e9dc1352a3335641d0c974367badcb2286b479
eastZoo <dongju010@naver.com>
2025-07-30 00:00:19 +0900

    UPDATE : 같은 탭에서 탭안의 또다른 탭 끼리의 상태는 마지막 상태가 저장되어 페이지 이동하더라도 마지막  페이지 기억

--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -1,18 +1,26 @@
-import { useRouter } from "expo-router";
+import { usePathname, useRouter } from "expo-router";
 import { Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   const router = useRouter();
+
+  const pathname = usePathname();
+
+  console.log("pathname", pathname);
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <View>
         <TouchableOpacity onPress={() => router.push(`/`)}>
-          <Text>For you</Text>
+          <Text style={{ color: pathname === "/" ? "red" : "black" }}>
+            For you
+          </Text>
         </TouchableOpacity>
       </View>
       <View>
         <TouchableOpacity onPress={() => router.push(`/following`)}>
-          <Text>Following</Text>
+          <Text style={{ color: pathname === "/" ? "black" : "red" }}>
+            Following
+          </Text>
         </TouchableOpacity>
       </View>
       <View>
