c7d126675da5226baa2b5fe39b459cbee969137f
eastZoo <dongju010@naver.com>
2025-07-29 23:57:51 +0900

    UPDATE : 비로그인 상태의 탭 클릭 시 로그인 요청 모달 띄우기 기능 구현

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,7 @@
 import { Ionicons } from "@expo/vector-icons";
 import { Tabs, useRouter } from "expo-router";
+import { useState } from "react";
+import { Modal, Text, TouchableOpacity, View } from "react-native";
 
 /**
  * - 아이콘 참고 사이트
@@ -7,85 +9,129 @@ import { Tabs, useRouter } from "expo-router";
  */
 export default function TabsLayout() {
   const router = useRouter();
+  const isLoggedIn = false;
+  const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
+
+  const openLoginModal = () => {
+    console.log("openLoginModal");
+    setIsLoginModalOpen(true);
+  };
+
+  const closeLoginModal = () => {
+    setIsLoginModalOpen(false);
+  };
 
   return (
-    <Tabs
-      screenOptions={{
-        headerShown: false,
-      }}
-    >
-      <Tabs.Screen
-        name="(home)"
-        options={{
-          tabBarLabel: () => null,
-          tabBarIcon: ({ focused }) => (
-            <Ionicons
-              name="home"
-              size={24}
-              color={focused ? "black" : "gray"}
-            />
-          ),
+    <>
+      <Tabs
+        screenOptions={{
+          headerShown: false,
         }}
-      />
-      <Tabs.Screen
-        name="search"
-        options={{
-          tabBarLabel: () => null,
-          tabBarIcon: ({ focused }) => (
-            <Ionicons
-              name="search"
-              size={24}
-              color={focused ? "black" : "gray"}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="add"
-        listeners={{
-          tabPress: (e) => {
-            // add는 페이지가 아닌 모달이 떠야하는 탭, 라우터 기능 방지
-            e.preventDefault();
-            router.navigate("/modal");
-          },
-        }}
-        options={{
-          tabBarLabel: () => null,
-          tabBarIcon: ({ focused }) => (
-            <Ionicons name="add" size={24} color={focused ? "black" : "gray"} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="activity"
-        options={{
-          tabBarLabel: () => null,
-          tabBarIcon: ({ focused }) => (
-            <Ionicons
-              name="heart-outline"
-              size={24}
-              color={focused ? "black" : "gray"}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="[username]"
-        options={{
-          tabBarLabel: () => null,
-          tabBarIcon: ({ focused }) => (
-            <Ionicons
-              name="person-outline"
-              size={24}
-              color={focused ? "black" : "gray"}
-            />
-          ),
-        }}
-      />
-      {/* 메인 화면에서 "/" 와 "/following" 페이지를 탭을통해 이동할 수 있는데 following은 탭에 표시하지 않음 
+      >
+        <Tabs.Screen
+          name="(home)"
+          options={{
+            tabBarLabel: () => null,
+            tabBarIcon: ({ focused }) => (
+              <Ionicons
+                name="home"
+                size={24}
+                color={focused ? "black" : "gray"}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="search"
+          listeners={{
+            tabPress: (e) => {
+              if (!isLoggedIn) {
+                e.preventDefault();
+                openLoginModal();
+              }
+            },
+          }}
+          options={{
+            tabBarLabel: () => null,
+            tabBarIcon: ({ focused }) => (
+              <Ionicons
+                name="search"
+                size={24}
+                color={focused ? "black" : "gray"}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="add"
+          listeners={{
+            tabPress: (e) => {
+              // add는 페이지가 아닌 모달이 떠야하는 탭, 라우터 기능 방지
+              e.preventDefault();
+              if (isLoggedIn) {
+                router.navigate("/modal");
+              } else {
+                openLoginModal();
+              }
+            },
+          }}
+          options={{
+            tabBarLabel: () => null,
+            tabBarIcon: ({ focused }) => (
+              <Ionicons
+                name="add"
+                size={24}
+                color={focused ? "black" : "gray"}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="activity"
+          listeners={{
+            tabPress: (e) => {
+              if (!isLoggedIn) {
+                e.preventDefault();
+                openLoginModal();
+              }
+            },
+          }}
+          options={{
+            tabBarLabel: () => null,
+            tabBarIcon: ({ focused }) => (
+              <Ionicons
+                name="heart-outline"
+                size={24}
+                color={focused ? "black" : "gray"}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="[username]"
+          listeners={{
+            tabPress: (e) => {
+              if (!isLoggedIn) {
+                e.preventDefault();
+                openLoginModal();
+              }
+            },
+          }}
+          options={{
+            tabBarLabel: () => null,
+            tabBarIcon: ({ focused }) => (
+              <Ionicons
+                name="person-outline"
+                size={24}
+                color={focused ? "black" : "gray"}
+              />
+            ),
+          }}
+        />
+        {/* 메인 화면에서 "/" 와 "/following" 페이지를 탭을통해 이동할 수 있는데 following은 탭에 표시하지 않음 
         href: null 옵션을 추가하면 탭에 표시되지 않음 하지만 그냥 (tabs) 폴더 안에 (home) 폴더 안에 index.tsx 파일을 만들어서 탭에 표시되지 않게 할 수 있음
       */}
-      {/* <Tabs.Screen
+        {/* <Tabs.Screen
         name="following"
         options={{
           tabBarLabel: () => null,
@@ -93,14 +139,41 @@ export default function TabsLayout() {
         }}
       /> */}
 
-      {/* 게시글 상세 페이지 탭 안보이게 추가*/}
-      <Tabs.Screen
-        name="(post)/[username]/post/[postID]"
-        options={{
-          tabBarLabel: () => null,
-          href: null,
-        }}
-      />
-    </Tabs>
+        {/* 게시글 상세 페이지 탭 안보이게 추가*/}
+        <Tabs.Screen
+          name="(post)/[username]/post/[postID]"
+          options={{
+            tabBarLabel: () => null,
+            href: null,
+          }}
+        />
+      </Tabs>
+
+      <Modal
+        visible={isLoginModalOpen}
+        transparent={true}
+        animationType="slide"
+        onRequestClose={closeLoginModal}
+      >
+        <View
+          style={{
+            flex: 1,
+            justifyContent: "flex-end",
+          }}
+        >
+          <View
+            style={{
+              backgroundColor: "white",
+              padding: 20,
+            }}
+          >
+            <Text>Login Modal</Text>
+            <TouchableOpacity onPress={closeLoginModal}>
+              <Ionicons name="close" size={24} color="#555" />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
+    </>
   );
 }
